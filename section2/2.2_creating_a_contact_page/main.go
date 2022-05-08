3d00fe023f78e8ed06e08bd446bd077ebaa08353
dduclayan <dduclayan@gmail.com>
2022-05-08 16:23:17 -0500

    submit new exercise

--- section2/2.2_creating_a_contact_page/main.go
+++ section2/2.2_creating_a_contact_page/main.go
@@ -5,13 +5,18 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email at <a href=\"mailto:devin@duclayan.com\">"+
+		"devin@duclayan.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
